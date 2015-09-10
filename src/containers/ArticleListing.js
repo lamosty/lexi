01005cbe639b05cf205efed943e0af0d957ae7c5
Rastislav Lamos <lamos.rasto@gmail.com>
2015-09-10 20:14:27 +0200

    Fix wrongly copied HTML -> React elem

--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -10,7 +10,7 @@ export default class ArticleListing extends Component {
                     <p className="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
                     <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                    <hr>
+                    <hr />
                         <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
                         <blockquote>
                             <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -36,7 +36,7 @@ export default class ArticleListing extends Component {
                             <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
                         </ol>
                         <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-                </div><!-- /.blog-post -->
+                </div>
 
                 <div className="blog-post">
                     <h2 className="blog-post-title">Another blog post</h2>
@@ -48,7 +48,7 @@ export default class ArticleListing extends Component {
                     </blockquote>
                     <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
                     <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                </div><!-- /.blog-post -->
+                </div>
 
                 <div className="blog-post">
                     <h2 className="blog-post-title">New feature</h2>
@@ -62,7 +62,7 @@ export default class ArticleListing extends Component {
                     </ul>
                     <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
                     <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                </div><!-- /.blog-post -->
+                </div>
 
                 <nav>
                     <ul className="pager">
